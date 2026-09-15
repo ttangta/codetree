1536c11b9f07f3100dff0595c2e70a76402ae76c
ttangta <kimjh6836@gmail.com>
2026-09-16 08:56:27 +0900

    [Passed] 사각형의 넓이 계속 계산하기 (51ms, 16MB)

---
+++ trail1/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.py
@@ -0,0 +1,16 @@
+while True:
+    arr = list(input().split())
+
+    a, b = int(arr[0]), int(arr[1])
+    c = arr[2]
+
+    result = a * b
+
+    if c == "C":
+        print(result)
+        break
+    
+    
+    print(result)
+
+    
