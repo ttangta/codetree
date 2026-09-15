34997f533da146d8219c6141641f6c8cd8b8eb92
ttangta <kimjh6836@gmail.com>
2026-09-15 21:48:04 +0900

    [Passed] 특정 조건의 온전수 구하기 (48ms, 16MB)

---
+++ trail1/특정 조건의 온전수 구하기/print-onjeonsu.py
@@ -0,0 +1,14 @@
+def solution(n):
+    result = []
+
+    for i in range(1, n+1):
+        if i % 2 == 0 or i % 10 == 5 or (i % 3 == 0 and i % 9 != 0):
+            continue
+        result.append(i)
+
+    return result
+
+n = int(input())
+
+print(*solution(n))
+
