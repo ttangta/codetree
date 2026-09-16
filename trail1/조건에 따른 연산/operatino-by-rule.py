4ded6193060c6f2b4c0da1410210f4d18db40da9
ttangta <kimjh6836@gmail.com>
2026-09-16 16:22:59 +0900

    [Passed] 조건에 따른 연산 (51ms, 16MB)

---
+++ trail1/조건에 따른 연산/operatino-by-rule.py
@@ -0,0 +1,15 @@
+def solution(n):
+    cnt = 0
+
+    while True:
+        if n >= 1000:
+            break
+        if n % 2 == 0:
+            n = (n * 3) + 1
+        else:
+            n = (n * 2) + 2
+        cnt += 1
+    return cnt
+
+n = int(input())
+print(solution(n))
