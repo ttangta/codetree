770fdadc683ab61f3650e5d403a8d9224414ad21
ttangta <kimjh6836@gmail.com>
2026-09-18 14:53:30 +0900

    [Passed] 별 그리기 (52ms, 16MB)

---
+++ trail1/별 그리기/star-drawing.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end="")
+    for j in range(i*2+1):
+        print("*",end="")
+    print()
+for i in range(n-1):
+    for j in range(i+1):
+        print(" ", end="")
+    for j in range((n*2) - (i*2+3)):
+        print("*",end="")
+    print()
