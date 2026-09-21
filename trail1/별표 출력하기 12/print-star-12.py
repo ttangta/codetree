cf9a5a86c81bdaeb1a03c735d3ae99a28ee4e869
ttangta <kimjh6836@gmail.com>
2026-09-21 14:01:26 +0900

    [Passed] 별표 출력하기 12 (50ms, 16MB)

---
+++ trail1/별표 출력하기 12/print-star-12.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n):
+        if i == 0 or (i <= j and j % 2 == 1):
+            print("*", end=" ")
+        else:
+            print(" ", end = " ")
+    print()
