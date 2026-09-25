916e90e2e2d6ebcf61ba55d25dea739782ec93bc
ttangta <kimjh6836@gmail.com>
2026-09-25 22:21:26 +0900

    [Passed] 19단 출력 (51ms, 16MB)

---
+++ trail1/19단 출력/nineteen-times-table.py
@@ -0,0 +1,10 @@
+for i in range(1, 20):
+    for j in range(1, 20):
+        if j%2 == 0:
+            print(f"{i} * {j} = {i * j}")
+        else:
+            if j < 19:
+                print(f"{i} * {j} = {i * j}", end = " / ")
+            else:
+                print(f"{i} * {j} = {i * j}", end="")
+    print()
